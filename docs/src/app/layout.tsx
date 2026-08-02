b6b3d8c42daf8c382abe756d93a9ae096065457a
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2026-08-03 00:24:29 +0200

    Refactor code structure for improved readability and maintainability

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,13 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import "./globals.css";
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
 import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
-import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "LWN Simulator Docs",
   description: "Documentation of LWN Simulator",
+  icons: ["./favicon.png"],
 };
 
 const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
@@ -24,7 +28,18 @@ export default async function RootLayout({
         <Layout
           navbar={
             <Navbar
-              logo={<h1 className="text-2xl">LWN Simulator</h1>}
+              logo={
+                <div className="flex items-center gap-2">
+                  <Image
+                    src="./favicon.png"
+                    alt="logo"
+                    width={50}
+                    height={50}
+                    className="w-12 h-12"
+                  />
+                  <h1 className="text-2xl">LWN Simulator</h1>
+                </div>
+              }
               projectLink="https://github.com/Giuseppe-Compagnone/LWN-Simulator"
             />
           }
